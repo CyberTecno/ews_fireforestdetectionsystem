1443e96ca40a931fdac8de84849e20516b75af89
Aavnxx <adikresna629@gmail.com>
2026-06-22 10:52:26 +0700

    Testing MQ-135

--- test.py
+++ test.py
@@ -1,20 +1,44 @@
-from gpiozero import Buzzer
+# from gpiozero import Buzzer
+# from time import sleep
+
+# BUZZER_PIN = 16
+
+# buzzer = Buzzer(BUZZER_PIN)
+
+# try:
+#     while True:
+#         print("Buzzer ON")
+#         buzzer.on()
+#         sleep(1)
+
+#         print("Buzzer OFF")
+#         buzzer.off()
+#         sleep(1)
+
+# except KeyboardInterrupt:
+#     buzzer.off()
+#     print("Program dihentikan")
+
+
+from gpiozero import DigitalInputDevice
 from time import sleep
 
-BUZZER_PIN = 16
+MQ135_PIN = 17
 
-buzzer = Buzzer(BUZZER_PIN)
+sensor = DigitalInputDevice(MQ135_PIN)
+
+print("Menunggu sensor pemanasan...")
+
+sleep(30)
 
 try:
     while True:
-        print("Buzzer ON")
-        buzzer.on()
-        sleep(1)
+        if sensor.value == 0:
+            print("⚠️ Gas/asap terdeteksi!")
+        else:
+            print("Udara normal")
 
-        print("Buzzer OFF")
-        buzzer.off()
         sleep(1)
 
 except KeyboardInterrupt:
-    buzzer.off()
     print("Program dihentikan")
