e3a1eb3a2ed9082884342da13377d1ad0067c950
Aavnxx <adikresna629@gmail.com>
2026-06-23 13:20:00 +0700

    Push Testing Buzzer

--- test.py
+++ test.py
@@ -1,44 +1,44 @@
-# from gpiozero import Buzzer
-# from time import sleep
-
-# BUZZER_PIN = 16
-
-# buzzer = Buzzer(BUZZER_PIN)
-
-# try:
-#     while True:
-#         print("Buzzer ON")
-#         buzzer.on()
-#         sleep(1)
-
-#         print("Buzzer OFF")
-#         buzzer.off()
-#         sleep(1)
-
-# except KeyboardInterrupt:
-#     buzzer.off()
-#     print("Program dihentikan")
-
-
-from gpiozero import DigitalInputDevice
+from gpiozero import Buzzer
 from time import sleep
 
-MQ135_PIN = 17
+BUZZER_PIN = 16
 
-sensor = DigitalInputDevice(MQ135_PIN)
-
-print("Menunggu sensor pemanasan...")
-
-sleep(30)
+buzzer = Buzzer(BUZZER_PIN)
 
 try:
     while True:
-        if sensor.value == 0:
-            print("⚠️ Gas/asap terdeteksi!")
-        else:
-            print("Udara normal")
+        print("Buzzer ON")
+        buzzer.on()
+        sleep(1)
 
+        print("Buzzer OFF")
+        buzzer.off()
         sleep(1)
 
 except KeyboardInterrupt:
+    buzzer.off()
     print("Program dihentikan")
+
+
+# from gpiozero import DigitalInputDevice
+# from time import sleep
+
+# MQ135_PIN = 17
+
+# sensor = DigitalInputDevice(MQ135_PIN)
+
+# print("Menunggu sensor pemanasan...")
+
+# sleep(30)
+
+# try:
+#     while True:
+#         if sensor.value == 0:
+#             print("⚠️ Gas/asap terdeteksi!")
+#         else:
+#             print("Udara normal")
+
+#         sleep(1)
+
+# except KeyboardInterrupt:
+#     print("Program dihentikan")
